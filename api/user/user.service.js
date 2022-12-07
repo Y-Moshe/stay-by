d475b9b89bfc3d9e5f58d60cdd21fb464176d4cb
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-07 18:36:09 +0200

    default user props

--- api/user/user.service.js
+++ api/user/user.service.js
@@ -89,7 +89,8 @@ async function add(user) {
             username: user.username,
             password: user.password,
             fullname: user.fullname,
-            imgUrl: user.imgUrl
+            imgUrl: user.imgUrl || 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png',
+            likedStays: []
         }
         const collection = await dbService.getCollection('user')
         await collection.insertOne(userToAdd)
