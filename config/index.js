ea4c26a5417659099949ff4f969f306b044e93a5
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-07 11:48:59 +0200

    setting mongodb connection string

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ var config
 if (process.env.NODE_ENV === 'production') {
   config = require('./prod')
 } else {
+  // config = require('./prod')
   config = require('./dev')
 }
 config.isGuestMode = true
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : 'tester_db'
+  dbURL: 'mongodb+srv://dbUser:y9ICahclUdIenYZ8@cluster0.xytksdp.mongodb.net/?retryWrites=true&w=majority',
+  dbName : 'stayby_db'
 }
