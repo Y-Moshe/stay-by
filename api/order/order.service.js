5cc33b5304616afc98eaf9d15c493871cc483773
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-07 18:09:31 +0200

    add createdAt with _mapOrder

--- api/order/order.service.js
+++ api/order/order.service.js
@@ -41,7 +41,7 @@ async function add(order) {
     try {
         const collection = await dbService.getCollection('order')
         await collection.insertOne(order)
-        return order
+        return _mapOrder(order)
     } catch (err) {
         logger.error('cannot insert order', err)
         throw err
